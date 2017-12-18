bcadd26f32513daf7e68e2213adbdef0672e7fce
ACken2 <acken2@outlook.com>
2017-12-18 08:12:01 +0800

    Added config for private local testnet

--- truffle-config.js
+++ truffle-config.js
@@ -3,4 +3,11 @@
  * Options can be found here: http://truffleframework.com/docs/advanced/configuration
  * */
 module.exports = {
+    networks: {
+        private: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*"
+        }
+    }
 };
