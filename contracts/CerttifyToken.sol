1a98e5c635dc48eebcb7d645312fa0ec8f152ed2
ACken2 <acken2@outlook.com>
2017-12-25 12:16:59 +0800

    Added a lock up on token transfer before ICO has ended

--- contracts/CerttifyToken.sol
+++ contracts/CerttifyToken.sol
@@ -17,9 +17,44 @@ contract CerttifyToken is StandardToken {
     string public symbol = "CTF";
     uint8 public decimals = 18;
 
+    address public deployer;
+    bool public lockup = true; 
+
     function CerttifyToken(uint256 maxSupply) public {
         totalSupply = maxSupply.mul(10 ** uint256(decimals));
         balances[msg.sender] = totalSupply;
+        deployer = msg.sender;
+    }
+
+    /**
+     * Modifier for function that can only be executed after lock up period
+     * During the lockup period, only contract deployer can execute those functions
+     */
+    modifier afterLockup() {
+        require(!lockup || msg.sender == deployer);
+        _;
+    }
+
+    /**
+     * Remove the lock up of tokens, can only be called by contract deployer
+     */
+    function unlock() public {
+        require(msg.sender == deployer);
+        lockup = false;
+    }
+
+    /**
+     * Impose lock up period on transfer() to block transfer of token during that period
+     */
+    function transfer(address _to, uint256 _value) public afterLockup() returns (bool) {
+        return super.transfer(_to, _value);
+    }
+
+    /**
+     * Impose lock up period on transferFrom() to block transfer of token during that period
+     */
+    function transferFrom(address _from, address _to, uint256 _value) public afterLockup() returns (bool) {
+        return super.transferFrom(_from, _to, _value);
     }
 
     /**
@@ -27,7 +62,7 @@ contract CerttifyToken is StandardToken {
      * @param _value The amount of token to be burned.
      * @param _wavesAddress The Waves address where converted token should be sent to.
      */
-    function burn(uint256 _value, string _wavesAddress) public {
+    function burn(uint256 _value, string _wavesAddress) public afterLockup() {
         require(_value > 0);
         require(_value <= balances[msg.sender]);
         // no need to require value <= totalSupply, since that would imply the
