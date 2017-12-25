dd65c9a12c91eba494b74f17094940f31860817c
ACken2 <acken2@outlook.com>
2017-12-25 13:30:40 +0800

    Added founderUnlock as parameter for deployment

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -9,8 +9,9 @@ module.exports = function(deployer, network, accounts) {
     const weiCostStage2 = web3.toBigNumber('11363000000000'); // 10% bouns, 1 ETH: 88,000 CTF
     const weiCostStage3 = web3.toBigNumber('12500000000000'); // 1 ETH: 80,000 CTF
     const wallet = '0x0AB00ec2b49EE42b09aee64f64f7E20804DC8960';
+    const founderUnlock = 1538352000; // 2018 Oct 01 00:00 GMT
     deployer.deploy(CerttifyCrowdsale, startStage1, startStage2, startStage3, endICO, 
         weiCostStage1, weiCostStage2, weiCostStage3,
-        wallet
+        wallet, founderUnlock
     );
 };
