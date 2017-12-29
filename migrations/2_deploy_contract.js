4f5688cb4092bb9e31b5f7804b710c6f3373be3e
ACken2 <acken2@outlook.com>
2017-12-29 18:32:52 +0800

    Edited deployment script to include unlock time of each phase

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -5,13 +5,19 @@ module.exports = function(deployer, network, accounts) {
     const startStage2 = 1520812800; // 2018 Mar 12 00:00 GMT
     const startStage3 = 1523232000; // 2018 Apr 09 00:00 GMT
     const endICO = 1523750400; // 2018 Apr 15 00:00 GMT
-    const weiCostStage1 = web3.toBigNumber('10417000000000'); // 20% bouns, 1 ETH: 96,000 CTF
-    const weiCostStage2 = web3.toBigNumber('11363000000000'); // 10% bouns, 1 ETH: 88,000 CTF
-    const weiCostStage3 = web3.toBigNumber('12500000000000'); // 1 ETH: 80,000 CTF
+    // 20% bouns, 1 ETH: 96,000 CTF
+    const weiCostStage1 = web3.toBigNumber(web3._extend.utils.toWei(1, 'ether')).div(web3.toBigNumber(96000));
+    // 10% bouns, 1 ETH: 88,000 CTF
+    const weiCostStage2 = web3.toBigNumber(web3._extend.utils.toWei(1, 'ether')).div(web3.toBigNumber(88000));
+     // 1 ETH: 80,000 CTF
+    const weiCostStage3 = web3.toBigNumber(web3._extend.utils.toWei(1, 'ether')).div(web3.toBigNumber(80000));
     const wallet = '0x0AB00ec2b49EE42b09aee64f64f7E20804DC8960';
-    const founderUnlock = 1538352000; // 2018 Oct 01 00:00 GMT
+    const founderUnlockPhase1 = 1538352000; // 2018 Oct 01 00:00 GMT
+    const founderUnlockPhase2 = 1569888000; // 2019 Oct 01 00:00 GMT
+    const founderUnlockPhase3 = 1601510400; // 2020 Oct 01 00:00 GMT
+    const founderUnlockPhase4 = 1633046400; // 2021 Oct 01 00:00 GMT
     deployer.deploy(CerttifyCrowdsale, startStage1, startStage2, startStage3, endICO, 
         weiCostStage1, weiCostStage2, weiCostStage3,
-        wallet, founderUnlock
+        wallet, founderUnlockPhase1, founderUnlockPhase2, founderUnlockPhase3, founderUnlockPhase4
     );
 };
