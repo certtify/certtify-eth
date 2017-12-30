4d371d38fcb90042c28dbed939f8a9a301c79030
ACken2 <acken2@outlook.com>
2017-12-30 17:54:22 +0800

    Edited the default cost of each token

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -5,12 +5,12 @@ module.exports = function(deployer, network, accounts) {
     const startStage2 = 1520812800; // 2018 Mar 12 00:00 GMT
     const startStage3 = 1523232000; // 2018 Apr 09 00:00 GMT
     const endICO = 1523750400; // 2018 Apr 15 00:00 GMT
-    // 20% bouns, 1 ETH: 96,000 CTF
-    const weiCostStage1 = web3.toBigNumber(web3._extend.utils.toWei(1, 'ether')).div(web3.toBigNumber(96000));
-    // 10% bouns, 1 ETH: 88,000 CTF
-    const weiCostStage2 = web3.toBigNumber(web3._extend.utils.toWei(1, 'ether')).div(web3.toBigNumber(88000));
-     // 1 ETH: 80,000 CTF
-    const weiCostStage3 = web3.toBigNumber(web3._extend.utils.toWei(1, 'ether')).div(web3.toBigNumber(80000));
+    // 20% bouns, 1 ETH: 24,000 CTF
+    const weiCostStage1 = web3.toBigNumber(web3._extend.utils.toWei(1, 'ether')).div(web3.toBigNumber(24000));
+    // 10% bouns, 1 ETH: 22,000 CTF
+    const weiCostStage2 = web3.toBigNumber(web3._extend.utils.toWei(1, 'ether')).div(web3.toBigNumber(22000));
+     // 1 ETH: 20,000 CTF
+    const weiCostStage3 = web3.toBigNumber(web3._extend.utils.toWei(1, 'ether')).div(web3.toBigNumber(20000));
     const wallet = '0x0AB00ec2b49EE42b09aee64f64f7E20804DC8960';
     const founderUnlockPhase1 = 1538352000; // 2018 Oct 01 00:00 GMT
     const founderUnlockPhase2 = 1569888000; // 2019 Oct 01 00:00 GMT
