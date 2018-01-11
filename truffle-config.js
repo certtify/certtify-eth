bd447b74c38778f7e1829f5e47368dafd3954ce6
ACken2 <acken2@outlook.com>
2018-01-11 20:59:47 +0800

    Switched to use infura.io for deployment

--- truffle-config.js
+++ truffle-config.js
@@ -2,15 +2,26 @@
  * Config file for truffle 
  * Options can be found here: http://truffleframework.com/docs/advanced/configuration
  * */
+
+var HDWalletProvider = require('truffle-hdwallet-provider');
+
+var infura_key = "";
+var mnemonic = "";
+
 module.exports = {
     networks: {
-        official: {
-            host: "localhost",
-            port: 8545,
-            network_id: "*",
-            gas: 6500000,
+        rinkeby: {
+			provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/" + infura_key),
+			network_id: 4,
+			gas: 6500000,
             gasPrice: 1000000000
-        },
+		},
+		mainnet: {
+			provider: new HDWalletProvider(mnemonic, "https://mainnet.infura.io/" + infura_key),
+			network_id: 1,
+			gas: 6000000,
+            gasPrice: 1000000000
+		},
         private: {
             host: "localhost",
             port: 8545,
