1224215e3037a0d7d5ce3333a9de7cba87948e10
ACken2 <acken2@outlook.com>
2018-02-07 16:08:56 +0800

    Added empty Bounty contract skeleton

---
+++ contracts/Bounty.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.4.18;
+/**
+ * @title Certtify Token Bounty Contract
+ * @author Ken Sze
+ * 
+ * @notice Bounty contract of Certtify token
+ */
+contract Bounty {
+
+    function Bounty() public {
+
+    }
+
+}
