7a71579fd32d3b8aecf4e11ebbcc33dfc51063df
ACken2 <acken2@outlook.com>
2018-02-13 13:27:37 +0800

    Updated migrations script to include owner field

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -12,12 +12,14 @@ module.exports = function(deployer, network, accounts) {
      // 1 ETH: 20,000 CTF
     const weiCostStage3 = web3.toBigNumber(web3._extend.utils.toWei(1, 'ether')).div(web3.toBigNumber(20000));
     const wallet = '0x0AB00ec2b49EE42b09aee64f64f7E20804DC8960';
+    const owner = "0x0AB00ec2b49EE42b09aee64f64f7E20804DC8960";
     const founderUnlockPhase1 = 1538352000; // 2018 Oct 01 00:00 GMT
     const founderUnlockPhase2 = 1569888000; // 2019 Oct 01 00:00 GMT
     const founderUnlockPhase3 = 1601510400; // 2020 Oct 01 00:00 GMT
     const founderUnlockPhase4 = 1633046400; // 2021 Oct 01 00:00 GMT
     deployer.deploy(CerttifyCrowdsale, startStage1, startStage2, startStage3, endICO, 
         weiCostStage1, weiCostStage2, weiCostStage3,
-        wallet, founderUnlockPhase1, founderUnlockPhase2, founderUnlockPhase3, founderUnlockPhase4
+        wallet, owner, 
+        founderUnlockPhase1, founderUnlockPhase2, founderUnlockPhase3, founderUnlockPhase4
     );
 };
