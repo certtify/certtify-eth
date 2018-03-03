9e801f3c1ee7cb9e46884df96e387d5b517c7698
ACken2 <acken2@outlook.com>
2018-03-03 17:46:42 +0800

    Finalized contract deployment parameters

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,25 +1,26 @@
 const CerttifyCrowdsale = artifacts.require('contracts/CerttifyCrowdsale.sol');
 
 module.exports = function(deployer, network, accounts) {
-    const startStage1 = 1518739200; // 2018 Feb 16 00:00 GMT
-    const startStage2 = 1520812800; // 2018 Mar 12 00:00 GMT
-    const startStage3 = 1523232000; // 2018 Apr 09 00:00 GMT
-    const endICO = 1523750400; // 2018 Apr 15 00:00 GMT
-    // 20% bouns, 1 ETH: 24,000 CTF
-    const weiCostStage1 = web3.toBigNumber(web3._extend.utils.toWei(1, 'ether')).div(web3.toBigNumber(24000));
-    // 10% bouns, 1 ETH: 22,000 CTF
-    const weiCostStage2 = web3.toBigNumber(web3._extend.utils.toWei(1, 'ether')).div(web3.toBigNumber(22000));
-     // 1 ETH: 20,000 CTF
-    const weiCostStage3 = web3.toBigNumber(web3._extend.utils.toWei(1, 'ether')).div(web3.toBigNumber(20000));
-    const wallet = '0x0AB00ec2b49EE42b09aee64f64f7E20804DC8960';
-    const owner = "0x0AB00ec2b49EE42b09aee64f64f7E20804DC8960";
-    const founderUnlockPhase1 = 1538352000; // 2018 Oct 01 00:00 GMT
-    const founderUnlockPhase2 = 1569888000; // 2019 Oct 01 00:00 GMT
-    const founderUnlockPhase3 = 1601510400; // 2020 Oct 01 00:00 GMT
-    const founderUnlockPhase4 = 1633046400; // 2021 Oct 01 00:00 GMT
+    const startStage1 = 1523145600; // 2018 Apr 08 00:00 GMT
+    const startStage2 = 1524355200; // 2018 Apr 22 00:00 GMT
+    const startStage3 = 1525564800; // 2018 May 06 00:00 GMT
+    const endICO = 1526774400; // 2018 May 20 00:00 GMT
+    // 20% bouns, 1 ETH: 48,000 CTF
+    const weiCostStage1 = web3.toBigNumber(web3._extend.utils.toWei(1, 'ether')).div(web3.toBigNumber(48000));
+    // 10% bouns, 1 ETH: 44,000 CTF
+    const weiCostStage2 = web3.toBigNumber(web3._extend.utils.toWei(1, 'ether')).div(web3.toBigNumber(44000));
+     // 1 ETH: 40,000 CTF
+    const weiCostStage3 = web3.toBigNumber(web3._extend.utils.toWei(1, 'ether')).div(web3.toBigNumber(40000));
+    const wallet = '0x102a7ce6f5755be0730f3ecfb05b8bfbe26e37c7';
+    const owner = "0x7e88309Bc199E8D83FdD8E7b8465068d6d83de53";
+    const bountyAdmin = "0x7126CB446EA014805ec56E5b620AFC41039c1958";
+    const founderUnlockPhase1 = 1533081600; // 2018 Aug 01 00:00 GMT
+    const founderUnlockPhase2 = 1554681600; // 2019 Apr 08 00:00 GMT
+    const founderUnlockPhase3 = 1586304000; // 2020 Apr 08 00:00 GMT
+    const founderUnlockPhase4 = 1617840000; // 2021 Apr 08 00:00 GMT
     deployer.deploy(CerttifyCrowdsale, startStage1, startStage2, startStage3, endICO, 
         weiCostStage1, weiCostStage2, weiCostStage3,
-        wallet, owner, 
+        wallet, owner, bountyAdmin,
         founderUnlockPhase1, founderUnlockPhase2, founderUnlockPhase3, founderUnlockPhase4
     );
 };
