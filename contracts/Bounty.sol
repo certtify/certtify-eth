2ce183cfe5e5fbf7aee85b6e76596fad57f00e1e
ACken2 <acken2@outlook.com>
2018-04-03 21:14:37 +0800

    Fixed some minor syntax issue

--- contracts/Bounty.sol
+++ contracts/Bounty.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.4.18;
 
-import './CerttifyToken.sol';
-import './Ownable.sol';
+import "./CerttifyToken.sol";
+import "./Ownable.sol";
 
 /**
  * @title Certtify Token Bounty Contract
--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -8,23 +8,23 @@ pragma solidity 0.4.18;
  */
 contract Ownable {
   
-  	address public owner;
+    address public owner;
 
-	/**
-	 * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-	 * account.
-	 */
-	function Ownable(address _owner) public {
-		owner = _owner;
-	}
+    /**
+        * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+        * account.
+        */
+    function Ownable(address _owner) public {
+        owner = _owner;
+    }
 
 
-	/**
-	 * @dev Throws if called by any account other than the owner.
-	 */
-	modifier onlyOwner() {
-		require(msg.sender == owner);
-		_;
-	}
+    /**
+        * @dev Throws if called by any account other than the owner.
+        */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
 }
